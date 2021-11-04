889477bdc3f7768993aff0e4e1a2075f1b84b504
paperfrog <paperfrog@naver.com>
2021-11-05 03:54:27 +0900

    Add shorten address feature
    처음에는 uuid로 접근했는데 uuid 너무 길어서 이대로는 안될 것 같아서 조사 후 서버타임 이용함

---
+++ src/main/java/paperfrog/urlshortener/Service/ServerTimeManager.java
@@ -0,0 +1,30 @@
+package paperfrog.urlshortener.Service;
+
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class ServerTimeManager {
+    /***
+     * 0 ~ 9 : 그대로 -> 10개
+     * 10 ~ 35 : a ~ z -> 26개
+     * 36 ~ 61 : A ~ Z -> 26개
+     * 62, 63 : # , @ -> 2개
+     ***/
+    public String getAddressByNanoTime(){
+        Long time=System.nanoTime();
+        System.out.println("time : "+time);
+        String address="";
+        while(time>0){
+            Long rem=time%64;
+            if(rem<10) address+=(char)('0'+rem);
+            else if(rem<36) address+=(char)('a'+rem-10);
+            else if(rem<61) address+=(char)('A'+rem-36);
+            else if(rem==62) address+='#';
+            else address+='@';
+            time/=64;
+        }
+        return address;
+    }
+}
